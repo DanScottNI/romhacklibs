bf3f0074603ef4b1e417ccc9c6dd24712b819d79
dan@millisle.plus.com <dan@millisle.plus.com@cb8149a2-d6ec-11de-829d-073b792d0d12>
2010-03-02 22:13:06 +0000

    Added a new Project namespace, which contains an interface for XML access, that a project should implement.

---
+++ trunk/Project/IXMLManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ROMHackLib.Project
+{
+    /// <summary>
+    /// Interface for the individual parts of a project to implement, so they
+    /// are persist their settings.
+    /// </summary>
+    /// <remarks>
+    /// Based on DahrkDaiz's IXMLIO interface.
+    /// </remarks>
+    public interface IXMLProject
+    {
+        /// <summary>
+        /// Creates an XML element.
+        /// </summary>
+        /// <returns>A XElement object that is used to persist the project's settings.</returns>
+        XElement CreateItem();
+
+        /// <summary>
+        /// Loads a project item from an XElement object.
+        /// </summary>
+        /// <param name="item">The XElement object to load the project item from.</param>
+        void LoadItem(XElement item);
+    }
+}
